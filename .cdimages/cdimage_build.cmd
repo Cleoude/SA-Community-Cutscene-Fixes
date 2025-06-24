7189e0b14ed3f7e899b2ecf419ca69f03d694c1f
mpPako <71462591+mpPako@users.noreply.github.com>
2025-06-25 00:04:59 +0200

    will i break everything on my first push?
    Place your bets ladies and gentelmans

--- .cdimages/cdimage_build.cmd
+++ .cdimages/cdimage_build.cmd
@@ -1,2 +1,7 @@
+mkdir ..\.cdimages\cuts_img
+copy ..\.cdimages\cuts_vanilla ..\.cdimages\cuts_img
+copy /y ..\.cdimages\cuts_edited ..\.cdimages\cuts_img
 cdimage_v2.exe "cuts_img"
 move /y cuts.img ..\modloader\CommunityCutsceneFixes\anim
+erase /q ..\.cdimages\cuts_img
+rd /q ..\.cdimages\cuts_img
